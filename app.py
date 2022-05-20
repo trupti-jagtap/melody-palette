cb9712bf598f8aac84dbe934aca3e7ce8236acfd
trupti-jagtap <trupti.598jagtap@gmail.com>
2022-05-20 23:50:34 +0530

    Major changes in frontend and flask app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,send_file
+from flask import Flask, render_template, send_file, flash
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
@@ -11,6 +11,7 @@ app = Flask(__name__)
 DOWNLOAD_FOLDER='static/output/mel.mid'
 app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['UPLOAD_FOLDER'] = 'static/files'
+ALLOWED_EXTENSIONS = {'mid'}
 # app.config['DOWNLOAD_FOLDER']='static/output'
 
 class UploadFileForm(FlaskForm):
@@ -18,37 +19,31 @@ class UploadFileForm(FlaskForm):
     submit = SubmitField("Upload File")
     #call the function melody generator
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/', methods=['GET',"POST"])
 @app.route('/home', methods=['GET',"POST"])
 def home():
-    response=''
-    recieved_input=''
+
     form = UploadFileForm()
     if form.validate_on_submit():
         file = form.file.data # First grab the file
         print("Input FIle is",file)
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
-        print("Calling generator")
-        initialize_generator()
-        return render_template('index.html',form=form, response='Done')
-    #recieved_input=file
+        if allowed_file(file.filename):
+            file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename)))
+            initialize_generator()
+            return render_template('index.html', form=form, response='Done')
+        else:
+            flash("Only MIDI files are allowed(.mid files)")# Then save the file
+        # print("Calling generator")
+
+
+
 
     return render_template('index.html', form=form,response='')
 
-# @app.route('/download')
-# def downloadFile ():
-#     #For windows you need to use drive name [ex: F:/Example.pdf]
-#     path = "/mel.mid"
-#     return send_file(path, as_attachment=True)
-# Download API
-# @app.route("/downloadfile/file", methods = ['GET'])
-# def download_file(file):
-#     return render_template('index.html',value=file)
-#
-# @app.route('/file')
-# def return_files_tut(filename):
-#     file_path = DOWNLOAD_FOLDER + filename
-#     return send_file(file_path, as_attachment=True, attachment_filename='')
 @app.route("/download/",methods=['POST'])
 def return_files_tut():
     file_path=DOWNLOAD_FOLDER
